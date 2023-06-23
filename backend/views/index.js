7125dcee3051a61d5c2b2468f246eb4d1cfa0762
HBK0077 <hrishikeshbalakrishna07@gmail.com>
2023-06-23 12:47:16 +0530

    ChatMessage displayed in frontend

--- backend/views/index.js
+++ backend/views/index.js
@@ -1,7 +1,29 @@
 let sendbtn = document.getElementById("send");
 let textMessage = document.getElementById("messages");
+let parentNode = document.getElementById("allMessages");
 
+//showing message from DB
+window.addEventListener("DOMContentLoaded", async(e)=>{
+    const token = localStorage.getItem("token");
+    const response = await axios.get("http://localhost:2000/show-message", {headers:{"Authorization": token}});
+    //console.log(response.data);
+    //console.log(response.data.allMessage.length);
+    for(let i=0;i<response.data.allMessage.length;i++){
+        showChatOnBrowser(response.data.allMessage[i]);
+    }
+})
 
+async function showChatOnBrowser(show){
+    try{
+        //console.log(show)
+        var childNode=`<li id=${show.id} style="margin-bottom:10px;">${show.message}</li>`
+            parentNode.innerHTML= parentNode.innerHTML+childNode;
+            console.log(parentNode);
+    }catch(err){
+        console.log(err);
+    }
+}
+//adding message ot DB
 sendbtn.onclick = async(e)=>{
     try{
         e.preventDefault();
